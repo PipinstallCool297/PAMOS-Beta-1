2ddd6dc659542d2cfcf8a7dbac3a5fd2729796ed
PipinstallCool297 <89711067+PipinstallCool297@users.noreply.github.com>
2022-07-07 19:44:17 -0700

    Update kernel.ino

--- kernel.ino
+++ kernel.ino
@@ -1,41 +1,73 @@
-// This is a list storing the names of the menu items
 String menuItems[] = {"System Info", "Graphics Test", "RedLED", "BlueLED", "GreenLED", "RGB-Nano"};
+
 int readKey;
 int savedDistance = 0;
+
+
 int menuPage = 0;
 int maxMenuPages = round(((sizeof(menuItems) / sizeof(String)) / 2) + .5);
 int cursorPosition = 0;
-// These byte arrays contain the graphics for the up and down arrow, along with the menu cursor
-byte downArrow[8] = {0b00100,0b00100,0b00100,0b00100,0b00100,0b10101,0b01110,0b00100};
-byte upArrow[8] = {0b00100,0b01110,0b10101,0b00100,0b00100,0b00100,0b00100,0b00100};
-byte menuCursor[8] = {B01000,B00100,B00010,B00001,B00010,B00100,B01000,B00000};
-// Here we are importing the modules for displaying text on the LCD Screen
+
+byte downArrow[8] = {
+  0b00100,   
+  0b00100, 
+  0b00100, 
+  0b00100, 
+  0b00100,    
+  0b10101, 
+  0b01110,  
+  0b00100 
+};
+
+byte upArrow[8] = {
+  0b00100, 
+  0b01110, 
+  0b10101,
+  0b00100, 
+  0b00100, 
+  0b00100, 
+  0b00100, 
+  0b00100  
+};
+
+byte menuCursor[8] = {
+  B01000, 
+  B00100, 
+  B00010, 
+  B00001,    
+  B00010,  
+  B00100, 
+  B01000, 
+  B00000  
+};
+
 #include <Wire.h>
 #include <LiquidCrystal.h>
-// This is initializing the pins that the LCD Display is connected to on the Arduino
+
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
-// This is the setup loop in whihc we initialize several items 
+
 void setup() {
-  // Here we are seting up these digital pins as output pins that provide HIGH or LOW Signals
   pinMode(23, OUTPUT);
   pinMode(25, OUTPUT);
   pinMode(27, OUTPUT);
   pinMode(14, OUTPUT);
-  // We are declaring that pin 14 will output a HIGH Signal
   digitalWrite(14, HIGH); 
-  // Now we are just turing on the serial monitor at 9600 Baud
   Serial.begin(9600);
-  // This is initializing the dimensions of the LCD Display at 16x2
+  
   lcd.begin(16, 2);
   lcd.clear();
-  // Here we are creating new characters using the byte arrays 
+
   lcd.createChar(0, menuCursor);
   lcd.createChar(1, upArrow);
-  lcd.createChar(2, downArrow);}
+  lcd.createChar(2, downArrow);
+}
+
 void loop() {
   mainMenuDraw();
   drawCursor();
-  operateMainMenu();}
+  operateMainMenu();
+}
+
 void mainMenuDraw() {
   Serial.print(menuPage);
   lcd.clear();
@@ -53,25 +85,41 @@ void mainMenuDraw() {
     lcd.write(byte(1));
   } else if (menuPage == maxMenuPages) {
     lcd.setCursor(15, 0);
-    lcd.write(byte(1));}}
+    lcd.write(byte(1));
+  }
+}
+
 void drawCursor() {
   for (int x = 0; x < 2; x++) {    
     lcd.setCursor(0, x);
-    lcd.print(" ");}
+    lcd.print(" ");
+  }
+
+  // The menu is set up to be progressive (menuPage 0 = Item 1 & Item 2, menuPage 1 = Item 2 & Item 3, menuPage 2 = Item 3 & Item 4), so
+  // in order to determine where the cursor should be you need to see if you are at an odd or even menu page and an odd or even cursor position.
   if (menuPage % 2 == 0) {
-    if (cursorPosition % 2 == 0) { 
+    if (cursorPosition % 2 == 0) {  // If the menu page is even and the cursor position is even that means the cursor should be on line 1
       lcd.setCursor(0, 0);
-      lcd.write(byte(0));}
-    if (cursorPosition % 2 != 0) {  
+      lcd.write(byte(0));
+    }
+    if (cursorPosition % 2 != 0) {  // If the menu page is even and the cursor position is odd that means the cursor should be on line 2
       lcd.setCursor(0, 1);
-      lcd.write(byte(0));}}
+      lcd.write(byte(0));
+    }
+  }
   if (menuPage % 2 != 0) {
-    if (cursorPosition % 2 == 0) {  
+    if (cursorPosition % 2 == 0) {  // If the menu page is odd and the cursor position is even that means the cursor should be on line 2
       lcd.setCursor(0, 1);
-      lcd.write(byte(0));}
-    if (cursorPosition % 2 != 0) {  
+      lcd.write(byte(0));
+    }
+    if (cursorPosition % 2 != 0) {  // If the menu page is odd and the cursor position is odd that means the cursor should be on line 1
       lcd.setCursor(0, 0);
-      lcd.write(byte(0));}}}
+      lcd.write(byte(0));
+    }
+  }
+}
+
+
 void operateMainMenu() {
   int activeButton = 0;
   while (activeButton == 0) {
@@ -79,14 +127,15 @@ void operateMainMenu() {
     readKey = analogRead(0);
     if (readKey < 790) {
       delay(100);
-      readKey = analogRead(0);}
+      readKey = analogRead(0);
+    }
     button = evaluateButton(readKey);
     switch (button) {
-      case 0:
+      case 0: // When button returns as 0 there is no action taken
         break;
-      case 1: 
+      case 1:  // This case will execute if the "forward" button is pressed
         button = 0;
-        switch (cursorPosition) { 
+        switch (cursorPosition) { // The case that is selected here is dependent on which menu page you are on and where the cursor is.
           case 0:
             menuItem1();
             break;
@@ -116,7 +165,8 @@ void operateMainMenu() {
             break;
           case 9:
             menuItem10();
-            break;}
+            break;
+        }
         activeButton = 1;
         mainMenuDraw();
         drawCursor();
@@ -125,15 +175,21 @@ void operateMainMenu() {
         button = 0;
         if (menuPage == 0) {
           cursorPosition = cursorPosition - 1;
-          cursorPosition = constrain(cursorPosition, 0, ((sizeof(menuItems) / sizeof(String)) - 1));}
+          cursorPosition = constrain(cursorPosition, 0, ((sizeof(menuItems) / sizeof(String)) - 1));
+        }
         if (menuPage % 2 == 0 and cursorPosition % 2 == 0) {
           menuPage = menuPage - 1;
-          menuPage = constrain(menuPage, 0, maxMenuPages);}
+          menuPage = constrain(menuPage, 0, maxMenuPages);
+        }
+
         if (menuPage % 2 != 0 and cursorPosition % 2 != 0) {
           menuPage = menuPage - 1;
-          menuPage = constrain(menuPage, 0, maxMenuPages);}
+          menuPage = constrain(menuPage, 0, maxMenuPages);
+        }
+
         cursorPosition = cursorPosition - 1;
         cursorPosition = constrain(cursorPosition, 0, ((sizeof(menuItems) / sizeof(String)) - 1));
+
         mainMenuDraw();
         drawCursor();
         activeButton = 1;
@@ -142,19 +198,25 @@ void operateMainMenu() {
         button = 0;
         if (menuPage % 2 == 0 and cursorPosition % 2 != 0) {
           menuPage = menuPage + 1;
-          menuPage = constrain(menuPage, 0, maxMenuPages);}
+          menuPage = constrain(menuPage, 0, maxMenuPages);
+        }
+
         if (menuPage % 2 != 0 and cursorPosition % 2 == 0) {
           menuPage = menuPage + 1;
-          menuPage = constrain(menuPage, 0, maxMenuPages);}
+          menuPage = constrain(menuPage, 0, maxMenuPages);
+        }
+
         cursorPosition = cursorPosition + 1;
         cursorPosition = constrain(cursorPosition, 0, ((sizeof(menuItems) / sizeof(String)) - 1));
         mainMenuDraw();
         drawCursor();
         activeButton = 1;
-        break;}}}
-// This is to detect when a button is pressed.
-// Since all the buttons are connected to A0...
-// The buttons detect spikes and drops in voltage levels to figure out which buttons are being pressed.
+        break;
+    }
+  }
+}
+
+// This function is called whenever a button press is evaluated. The LCD shield works by observing a voltage drop across the buttons all hooked up to A0.
 int evaluateButton(int x) {
   int result = 0;
   if (x < 50) {
@@ -164,36 +226,52 @@ int evaluateButton(int x) {
   } else if (x < 380) {
     result = 3; // down
   } else if (x < 790) {
-    result = 4; // left}
-  return result;}
+    result = 4; // left
+  }
+  return result;
+}
+
+// If there are common usage instructions on more than 1 of your menu items you can call this function from the sub
+// menus to make things a little more simplified. If you don't have common instructions or verbage on multiple menus
+// I would just delete this void. You must also delete the drawInstructions()function calls from your sub menu functions.
 void drawInstructions() {
-  lcd.setCursor(0, 1); // Setting the cursor to the second line of the display
+  lcd.setCursor(0, 1); // Set cursor to the bottom line
   lcd.print("Use ");
-  lcd.write(byte(1)); // Displaying the up arrow character
+  lcd.write(byte(1)); // Up arrow
   lcd.print("/");
-  lcd.write(byte(2)); // Displaying the down arrow character
-  lcd.print(" buttons");}
+  lcd.write(byte(2)); // Down arrow
+  lcd.print(" buttons");
+}
+
 void menuItem1() {
   int activeButton = 0;
+
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("Pamos-Beta v.2.1");
   lcd.setCursor(0,1);
-  lcd.print("VID:2341PID:0042");
+    lcd.print("VID:2341PID:0042");
+
   while (activeButton == 0) {
     int button;
     readKey = analogRead(0);
     if (readKey < 790) {
       delay(100);
-      readKey = analogRead(0);}
+      readKey = analogRead(0);
+    }
     button = evaluateButton(readKey);
     switch (button) {
       case 4: 
         button = 0;
         activeButton = 1;
-        break;}}}
-void menuItem2() { 
+        break;
+    }
+  }
+}
+
+void menuItem2() { // Function executes when you select the 2nd item from main menu
   int activeButton = 0;
+
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("1000000000000000");
@@ -332,15 +410,21 @@ void menuItem2() {
     readKey = analogRead(0);
     if (readKey < 790) {
       delay(100);
-      readKey = analogRead(0);}
+      readKey = analogRead(0);
+    }
     button = evaluateButton(readKey);
     switch (button) {
-      case 4:  
+      case 4:  // This case will execute if the "back" button is pressed
         button = 0;
         activeButton = 1;
-        break;}}}
-void menuItem3() { 
+        break;
+    }
+  }
+}
+
+void menuItem3() { // Function executes when you select the 3rd item from main menu
   int activeButton = 0;
+
   lcd.clear();
   lcd.setCursor(3, 0);
   digitalWrite(27, HIGH); 
@@ -350,17 +434,24 @@ void menuItem3() {
     readKey = analogRead(0);
     if (readKey < 790) {
       delay(100);
-      readKey = analogRead(0);}
+      readKey = analogRead(0);
+    }
     button = evaluateButton(readKey);
     switch (button) {
-      case 4: 
+      case 4:  // This case will execute if the "back" button is pressed
         button = 0;
         activeButton = 1;
-        break;}}}
-void menuItem4() { 
+        break;
+    }
+  }
+}
+
+void menuItem4() { // Function executes when you select the 4th item from main menu
   int activeButton = 0;
+
   lcd.clear();
   lcd.setCursor(3, 0);
+   
   digitalWrite(23, HIGH);
   lcd.print("BlueLED ON");
   while (activeButton == 0) {
@@ -368,15 +459,21 @@ void menuItem4() {
     readKey = analogRead(0);
     if (readKey < 790) {
       delay(100);
-      readKey = analogRead(0);}
+      readKey = analogRead(0);
+    }
     button = evaluateButton(readKey);
     switch (button) {
-      case 4: 
+      case 4:  // This case will execute if the "back" button is pressed
         button = 0;
         activeButton = 1;
-        break;}}}
-void menuItem5() { 
+        break;
+    }
+  }
+}
+
+void menuItem5() { // Function executes when you select the 5th item from main menu
   int activeButton = 0;
+
   lcd.clear();
   lcd.setCursor(3, 0);
   digitalWrite(25, HIGH); 
@@ -386,15 +483,21 @@ void menuItem5() {
     readKey = analogRead(0);
     if (readKey < 790) {
       delay(100);
-      readKey = analogRead(0);}
+      readKey = analogRead(0);
+    }
     button = evaluateButton(readKey);
     switch (button) {
-      case 4:  
+      case 4:  // This case will execute if the "back" button is pressed
         button = 0;
         activeButton = 1;
-        break;}}}
-void menuItem6() {
+        break;
+    }
+  }
+}
+
+void menuItem6() { // Function executes when you select the 6th item from main menu
   int activeButton = 0;
+
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("RGB-Nano Booted");
@@ -404,79 +507,111 @@ void menuItem6() {
     readKey = analogRead(0);
     if (readKey < 790) {
       delay(100);
-      readKey = analogRead(0);}
+      readKey = analogRead(0);
+    }
     button = evaluateButton(readKey);
     switch (button) {
-      case 4:  
+      case 4:  // This case will execute if the "back" button is pressed
         button = 0;
         activeButton = 1;
-        break;}}}
-void menuItem7() { 
+        break;
+    }
+  }
+}
+
+void menuItem7() { // Function executes when you select the 7th item from main menu
   int activeButton = 0;
+
   lcd.clear();
   lcd.setCursor(3, 0);
   lcd.print("Sub Menu 7");
+
   while (activeButton == 0) {
     int button;
     readKey = analogRead(0);
     if (readKey < 790) {
       delay(100);
-      readKey = analogRead(0);}
+      readKey = analogRead(0);
+    }
     button = evaluateButton(readKey);
     switch (button) {
-      case 4: 
+      case 4:  // This case will execute if the "back" button is pressed
         button = 0;
         activeButton = 1;
-        break;}}}
-void menuItem8() {
+        break;
+    }
+  }
+}
+
+void menuItem8() { // Function executes when you select the 8th item from main menu
   int activeButton = 0;
+
   lcd.clear();
   lcd.setCursor(3, 0);
   lcd.print("Sub Menu 8");
+
   while (activeButton == 0) {
     int button;
     readKey = analogRead(0);
     if (readKey < 790) {
       delay(100);
-      readKey = analogRead(0);}
+      readKey = analogRead(0);
+    }
     button = evaluateButton(readKey);
     switch (button) {
-      case 4: 
+      case 4:  // This case will execute if the "back" button is pressed
         button = 0;
         activeButton = 1;
-        break;}}}
-void menuItem9() { 
+        break;
+    }
+  }
+}
+
+void menuItem9() { // Function executes when you select the 9th item from main menu
   int activeButton = 0;
+
   lcd.clear();
   lcd.setCursor(3, 0);
   lcd.print("Sub Menu 9");
+
   while (activeButton == 0) {
     int button;
     readKey = analogRead(0);
     if (readKey < 790) {
       delay(100);
-      readKey = analogRead(0);}
+      readKey = analogRead(0);
+    }
     button = evaluateButton(readKey);
     switch (button) {
-      case 4: 
+      case 4:  // This case will execute if the "back" button is pressed
         button = 0;
         activeButton = 1;
-        break;}}}
-void menuItem10() { 
+        break;
+    }
+  }
+}
+
+void menuItem10() { // Function executes when you select the 10th item from main menu
   int activeButton = 0;
+
   lcd.clear();
   lcd.setCursor(3, 0);
-  lcd.print("Arduino Mega 2560");
+  lcd.print("Arduino Mega 2560 v.2.1");
   lcd.print("VID: 2341 PID: 0042");
+
   while (activeButton == 0) {
     int button;
     readKey = analogRead(0);
     if (readKey < 790) {
       delay(100);
-      readKey = analogRead(0);}
+      readKey = analogRead(0);
+    }
     button = evaluateButton(readKey);
     switch (button) {
-      case 4: 
+      case 4:  // This case will execute if the "back" button is pressed
         button = 0;
         activeButton = 1;
-        break;}}}
+        break;
+    }
+  }
+}
